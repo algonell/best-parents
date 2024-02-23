64cf7fa6b8631b8c1f41cb9d553e695c6d02b86f
void <algonell@gmail.com>
2024-02-23 20:30:02 +0200

    chore: avoid backup creation on update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Maven..."
-mvn -f ~/git/best-parents versions:use-latest-releases
+mvn -f ~/git/best-parents versions:use-latest-releases -DgenerateBackupPoms=false
 
 echo "build..."
 ~/git/best-parents/build.sh
