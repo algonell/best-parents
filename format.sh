8f1505dd78fc6c71f271a3d29c3c82beef8c4172
void <algonell@gmail.com>
2024-02-24 11:41:13 +0200

    style: format

--- format.sh
+++ format.sh
@@ -2,10 +2,10 @@
 
 clear
 
+echo
 echo "google-java-format..."
 java -jar ~/Misc/google-java-format-*.jar --replace $(git ls-files ~/git/best-parents/*.java)
 
 echo
-
 echo "SpotBugs..."
 mvn -f ~/git/best-parents spotbugs:check
