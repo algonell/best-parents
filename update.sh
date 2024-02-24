1952e9bd0a4f5e17d3bc5d302d41aa9f91018bb7
void <algonell@gmail.com>
2024-02-24 12:07:35 +0200

    feat: format XML on update

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 echo "Maven..."
 mvn -f ~/git/best-parents versions:use-latest-releases -DgenerateBackupPoms=false
+find ~/git/best-parents -name "*.xml" -exec xmllint --format '{}' --output '{}' \;
 
 echo "build..."
 ~/git/best-parents/build.sh
